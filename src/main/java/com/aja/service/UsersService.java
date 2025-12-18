7eb5729a058d32049fa5c3642cb30b8818ab137c
Devasena753 <d.medavaram@ajacs.in>
2025-12-18 12:31:10 +0530

    s

--- src/main/java/com/aja/service/UsersService.java
+++ src/main/java/com/aja/service/UsersService.java
@@ -6,7 +6,7 @@ import com.aja.entity.Users;
 
 public interface UsersService {
 	
-	 public Users registerUsers(Users user);
+	   public Users registerUsers(Users user);
 	   public List<Users> getAllUsers();
 	   public Users login(String email, String password);
 	   public Users updateUser(Long id, Users user);
