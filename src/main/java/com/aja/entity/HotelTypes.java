5c15a3f979a3aeb419cd45b23e9a8367222e1e40
manishm72 <m.maharana@ajacs.in>
2025-12-29 17:35:32 +0530

    s

--- src/main/java/com/aja/entity/HotelTypes.java
+++ src/main/java/com/aja/entity/HotelTypes.java
@@ -1,21 +1,16 @@
 package com.aja.entity;
 
 import java.util.ArrayList;
-
 import java.util.List;
 
 import jakarta.persistence.Entity;
-
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
-
 import jakarta.persistence.GenerationType;
-
 import jakarta.persistence.Id;
-
 import jakarta.persistence.OneToMany;
-
 import lombok.Getter;
-
 import lombok.Setter;
 
 @Entity
@@ -35,7 +30,9 @@ public class HotelTypes {
 	private String htypeName;
 
 	private Double pricePerday;
-
+    
+	@Enumerated(EnumType.STRING)
+	private Meals meals;
 	@OneToMany(mappedBy = "hoteltype")
 
 	private List<Bookings> bookings = new ArrayList<>();
